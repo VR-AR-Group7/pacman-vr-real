e40ea1fd8c6136ce84c35bf65a8fb4ab7edafe29
Kenneth Louis <lowkam4@gmail.com>
2022-12-14 20:38:04 -0500

    finished with bugs

---
+++ VRGame/Assets/Scripts/IncreaseSpeedKen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseSpeedKen : MonoBehaviour
+{
+    private float speedI;
+    // Start is called before the first frame update
+    void Start()
+    {
+     //   moving = false
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //void OnTriggerEnter(Collider other){
+      // if(other.tag ='Speedincrease')
+      // {
+       //    speedI=7;
+        //}
+   // }
+}
---
+++ VRGame/Assets/Scripts/deathKen.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class deathKen : MonoBehaviour
+////
+ 
+  //  public deathKen over;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //}
+
+    // Update is called once per frame
+    //void Update()
+   // {
+        //if(GameObject.FindGameObjectsWithTag("monster").Length == 0){
+          //  Debug.Log("You Win!");
+          //  GameOver();
+
+        //}
+  ////  }
+    // public void GameOver(){
+       //Debug.Log("Game Over");
+        //gameover.Setup();
+  //  }
+//}
